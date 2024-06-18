370a4faa21366868ec520575cc706e5a83d2119d
Sahadev Padavale <padavalesahadev2612@gmail.com>
2024-06-18 17:12:48 +0530

    added role based authorization

---
+++ Server/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using SiGaHRMS.Data.Model.Dto;
+
+namespace SiGaHRMS.ApiService.Interfaces;
+
+public interface IAuthService
+{
+    Task<string> Register(RegistrationRequestDto registrationRequestDto);
+    Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+    Task<bool> AssignRole(string email, string roleName);
+    Task<bool> CreateRole(string roleName);
+}
