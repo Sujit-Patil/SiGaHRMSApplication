04d14499c0e7d7ab6f04610d6b55df06695b9bcc
Sujit-Patil <131465018+Sujit-Patil@users.noreply.github.com>
2024-06-19 17:19:42 +0530

    Added error contants

---
+++ Data/Validations/ValidationResult.cs
@@ -0,0 +1,26 @@
+﻿namespace SiGaHRMS.Data.Validations
+{
+    public class ValidationResult
+    {
+        public bool IsValid => Errors.Count < 1;
+
+        public List<ErrorMessage> Errors { get; set; } = new List<ErrorMessage>();
+
+        public void AddErrorMesageCode(string code, IDictionary<string, string> errors, params string[] args)
+        {
+            string errorMessage = string.Empty;
+
+            if (errors.ContainsKey(code))
+            {
+                errorMessage = string.Format(errors[code], args);
+            }
+
+            Errors.Add(
+                new ErrorMessage
+                {
+                    Code = code,
+                    Message = errorMessage,
+                });
+        }
+    }
+}
